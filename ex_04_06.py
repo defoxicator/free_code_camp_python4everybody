0ede83794f44d78b8ab77447588114d2f9d7b22e
Albert <104618176+defoxicator@users.noreply.github.com>
2022-05-10 14:00:46 +0200

    Update ex_04_06.py
    Changed the form of file. Function is called for 42 hours with payment rate value = 42

--- ex_04_06.py
+++ ex_04_06.py
@@ -1,23 +1,18 @@
 # Excercise 04_06
 # Rewriting the Excercise 03_02 to form a function
 
-def computepay():
-    loop = 0
-    while loop == 0:
-        try:
-            hours = float(input("Enter no. of hours: "))
-            rate = float(input("Enter payment per hour: "))
-            loop += 1
-        except:
-            print("Please enter numeric input.")
-
-    if hours > 40:
-        hours_above = hours - 40
-        bonus_rate = rate * 1.5
-        bonus_payment = hours_above * bonus_rate
-    else:
-        bonus_payment = 0
+def computepay(hours, rate):
+    try:
+        if hours > 40:
+            hours_above = hours - 40
+            bonus_rate = rate * 1.5
+            bonus_payment = hours_above * bonus_rate
+        else:
+            bonus_payment = 0
 
         print("Your pay:", hours * rate + bonus_payment)
 
-computepay()
+    except:
+            print("Please enter numeric input.")
+
+computepay(42, 42)
